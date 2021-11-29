d1c23fed94c0a907f2ed0382c4a7cf035adc4dc0
Mahesh Jada <mjada@innominds.com>
2021-11-29 16:09:58 +0530

    design is updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupComponent } from './signup/signup.component';
@@ -9,8 +10,10 @@ import {CategoriesComponent} from './categories/categories.component';
 const routes: Routes = [
   {path:'',component:HomePageComponent},
     {path:'login',component:LoginPageComponent},
+    {path:'Forgot',component:ForgotPasswordComponent},
     { path:'categories',component:CategoriesComponent},
     {path:'signup',component:SignupComponent}
+
 ];
 
 @NgModule({
